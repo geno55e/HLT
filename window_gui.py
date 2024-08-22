c9405b0efc8fcf0abd2a652f06ff898371576792
geno55e <anton132@hotmail.de>
2024-08-22 02:53:42 +0200

    VISA_classes.py hinzugefügt

--- window_gui.py
+++ window_gui.py
@@ -1,175 +1,280 @@
+import pyvisa
 import tkinter as tk
 from tkinter import ttk
 from plot import plot
+from time import sleep
+
+
+# HM8143 Spannungsquelle
+def HM8143_Quelle_remoteOn():
+    rm = pyvisa.ResourceManager()
+    my_instrument = rm.open_resource('ASRL6::INSTR', write_termination='\r', read_termination='\r')
+    my_instrument.write('RM1')
+    my_instrument.close()
+
+
+def HM8143_Quelle_remoteOff():
+    rm = pyvisa.ResourceManager()
+    my_instrument = rm.open_resource('ASRL6::INSTR', write_termination='\r', read_termination='\r')
+    my_instrument.write('RM0')
+    my_instrument.close()
+
+
+def HM8143_Quelle_SpannungLinks(spannung):
+    rm = pyvisa.ResourceManager()
+    my_instrument = rm.open_resource('ASRL6::INSTR', write_termination='\r', read_termination='\r')
+    my_instrument.write('SU1:'+str(spannung))
+    my_instrument.close()
+
+
+def HM8143_Quelle_SpannungRechts(spannung):
+    rm = pyvisa.ResourceManager()
+    my_instrument = rm.open_resource('ASRL6::INSTR', write_termination='\r', read_termination='\r')
+    my_instrument.write('SU2:'+str(spannung))
+    my_instrument.close()
+
+
+def HM8143_Quelle_StromLinks(strom):
+    rm = pyvisa.ResourceManager()
+    my_instrument = rm.open_resource('ASRL6::INSTR', write_termination='\r', read_termination='\r')
+    my_instrument.write('SI1:'+str(strom))
+    my_instrument.close()
+
+
+def HM8143_Quelle_StromRechts(strom):
+    rm = pyvisa.ResourceManager()
+    my_instrument = rm.open_resource('ASRL6::INSTR', write_termination='\r', read_termination='\r')
+    my_instrument.write('SI2:'+str(strom))
+    my_instrument.close()
+
+
+def HM8143_Quelle_AusgangOn():
+    rm = pyvisa.ResourceManager()
+    my_instrument = rm.open_resource('ASRL6::INSTR', write_termination='\r', read_termination='\r')
+    my_instrument.write('OP1')
+    my_instrument.close()
+
+
+def HM8143_Quelle_AusgangOff():
+    rm = pyvisa.ResourceManager()
+    my_instrument = rm.open_resource('ASRL6::INSTR', write_termination='\r', read_termination='\r')
+    my_instrument.write('OP0')
+    my_instrument.close()
+
+
+def HM8143_Quelle_Toggle_Ausgang():
+    if Button_on_off_HM8143_Quelle.cget("text") == 'Off':
+        HM8143_Quelle_AusgangOn()
+        Button_on_off_HM8143_Quelle["text"] = "On"
+    else:
+        HM8143_Quelle_AusgangOff()
+        Button_on_off_HM8143_Quelle["text"] = "Off"
+
+
+# Fluke
+def Fluke_LeseGleichspannung(messbereich):
+    rm = pyvisa.ResourceManager()
+    my_instrument = rm.open_resource('ASRL5::INSTR', read_termination = '\r\n', query_delay = 0.21)
+    my_instrument.write('*RST;*CLS;CONF:VOLT:DC '+str(messbereich)+';:VOLT:DC:NPLC 1;:TRIG:SOUR BUS')
+    spannung = my_instrument.query(':INIT;*TRG;FETCH?')
+    my_instrument.write('*RST;*CLS;syst:local')
+    my_instrument.close()
+    return spannung
+
 
 window_height = 680
 window_width = 1065
 
-'Instanziere das Hauptfenster'
+
+# ##########################################################################
+
+# Instanziiere das Hauptfenster'
+
 master = tk.Tk()
+master.geometry("1065x500")
+
+Frame_Steuerung = ttk.Frame(master)
+Frame_Plot = ttk.Frame(master)
+
+Frame_Steuerung.place(x=0, y=0, relwidth=0.4, relheight=1)
+Frame_Plot.place(relx=0.4, y=0, relwidth=0.6, relheight=1)
+
 
 # Lokal
-Frame_Geraete_lokal = tk.Frame(master, highlightthickness=1)
-Button_Geraete_lokal = tk.Button(master, text="Geräte lokal bedienen")
+Frame_Geraete_lokal = ttk.Frame(master)
+Button_Geraete_lokal = ttk.Button(master, text="Geräte lokal bedienen")
 
-# HM8143
-Frame_HM8143_Quelle = tk.LabelFrame(master, text="Spannungsquelle HM8143")
-Label_Ueberschrift_HM8143_Quelle = tk.Label(master, text="Spannungsquelle HM8143")
 
-Label_Spannung_links_HM8143_Quelle = tk.Label(master, text="Spannung links")
-Label_Spannung_rechts_HM8143_Quelle = tk.Label(master, text="Spannung rechts")
+# HM8143 Power
+Frame_HM8143_Quelle = ttk.LabelFrame(Frame_Steuerung, text="Spannungsquelle HM8143")
 
-Eingabe_Spannung_links_HM8143_Quelle = tk.Entry(master)
-Eingabe_Spannung_links_HM8143_Quelle.insert(0, "0")
-Eingabe_Spannung_rechts_HM8143_Quelle = tk.Entry(master)
+Label_Spannung_links_HM8143_Quelle = ttk.Label(Frame_HM8143_Quelle, text="Spannung links")
+Label_Spannung_rechts_HM8143_Quelle = ttk.Label(Frame_HM8143_Quelle, text="Spannung rechts")
+
+Eingabe_Spannung_links_HM8143_Quelle = ttk.Entry(Frame_HM8143_Quelle, width=7)
+Eingabe_Spannung_links_HM8143_Quelle.insert(0, "0,095")
+Eingabe_Spannung_links_HM8143_Quelle.bind("<Return>", (lambda event: HM8143_Quelle_SpannungLinks(Eingabe_Spannung_links_HM8143_Quelle.get())))
+Eingabe_Spannung_rechts_HM8143_Quelle = ttk.Entry(Frame_HM8143_Quelle, width=7)
 Eingabe_Spannung_rechts_HM8143_Quelle.insert(0, "0")
+Eingabe_Spannung_rechts_HM8143_Quelle.bind("<Return>", (lambda event: HM8143_Quelle_SpannungRechts(Eingabe_Spannung_rechts_HM8143_Quelle.get())))
 
-Label_Strom_links_HM8143_Quelle = tk.Label(master, text="Strom links")
-Label_Strom_rechts_HM8143_Quelle = tk.Label(master, text="Strom rechts")
+Label_Strom_links_HM8143_Quelle = ttk.Label(Frame_HM8143_Quelle, text="Strom links")
+Label_Strom_rechts_HM8143_Quelle = ttk.Label(Frame_HM8143_Quelle, text="Strom rechts")
 
-Eingabe_Strom_links_HM8143_Quelle = tk.Entry(master)
+Eingabe_Strom_links_HM8143_Quelle = ttk.Entry(Frame_HM8143_Quelle, width=7)
 Eingabe_Strom_links_HM8143_Quelle.insert(0,"0.095")
-Button_on_off_HM8143_Quelle = tk.Button(master, text="Off")
-Eingabe_Strom_rechts_HM8143_Quelle = tk.Entry(master)
+Eingabe_Strom_links_HM8143_Quelle.bind("<Return>", (lambda event: HM8143_Quelle_StromLinks(Eingabe_Strom_links_HM8143_Quelle.get())))
+Button_on_off_HM8143_Quelle = ttk.Button(Frame_HM8143_Quelle, text="Off", command=HM8143_Quelle_Toggle_Ausgang)
+Eingabe_Strom_rechts_HM8143_Quelle = ttk.Entry(Frame_HM8143_Quelle, width=7)
 Eingabe_Strom_rechts_HM8143_Quelle.insert(0,"0.095")
+Eingabe_Strom_rechts_HM8143_Quelle.bind("<Return>", (lambda event: HM8143_Quelle_StromRechts(Eingabe_Strom_rechts_HM8143_Quelle.get())))
 
+# HM8143 Design
+Frame_HM8143_Quelle.pack()
+Label_Spannung_links_HM8143_Quelle.grid(column=0, row=0, sticky="W", padx=5, pady=1)
+Label_Spannung_rechts_HM8143_Quelle.grid(column=2, row=0, sticky="E", padx=5, pady=1)
+Eingabe_Spannung_links_HM8143_Quelle.grid(column=0, row=1, sticky="W", padx=5, pady=1)
+Eingabe_Spannung_rechts_HM8143_Quelle.grid(column=2, row=1, sticky="E", padx=5, pady=1)
 
-# HM8150
-Label_Ueberschrift_HM8150_Freq = tk.Label(master, text="Funktionsgenerator HM8150")
+Label_Strom_links_HM8143_Quelle.grid(column=0, row=2, sticky="W", padx=5, pady=1)
+Label_Strom_rechts_HM8143_Quelle.grid(column=2, row=2, sticky="E", padx=5, pady=1)
+Eingabe_Strom_links_HM8143_Quelle.grid(column=0, row=3, sticky="W", padx=5, pady=1)
+Button_on_off_HM8143_Quelle.grid(column=1, row=3, padx=5, pady=1)
+Eingabe_Strom_rechts_HM8143_Quelle.grid(column=2, row=3, sticky="E",  padx=5, pady=1)
 
-Label_Wellenform_HM8150_Freq = tk.Label(master, text="Wellenform")
-Label_Amplitude_HM8150_Freq = tk.Label(master, text="Amplitude")
-Label_Frequenz_HM8150_Freq = tk.Label(master, text="Frequenz")
-Label_Offset_HM8150_Freq = tk.Label(master, text="Offset")
+# HM8150 Frequenzgenerator
+Frame_HM8150_Freq = ttk.LabelFrame(Frame_Steuerung, text="Funktionsgenerator HM8150")
+
+Label_Wellenform_HM8150_Freq = ttk.Label(Frame_HM8150_Freq, text="Wellenform")
+Label_Amplitude_HM8150_Freq = ttk.Label(Frame_HM8150_Freq, text="Amplitude")
+Label_Frequenz_HM8150_Freq = ttk.Label(Frame_HM8150_Freq, text="Frequenz")
+Label_Offset_HM8150_Freq = ttk.Label(Frame_HM8150_Freq, text="Offset")
 
 Combo_Wellenform_HM8150_Freq = ttk.Combobox(
-    master,
+    Frame_HM8150_Freq,
     state="readonly",
-    values=["Sinus", "Rechteck", "Dreieck", "Puls", "Sägezahn"]
+    values=["Sinus", "Rechteck", "Dreieck", "Puls", "Sägezahn"],
+    width=13
 )
 Combo_Wellenform_HM8150_Freq.current(0)
 
-Eingabe_Amplitude_HM8150_Freq = tk.Entry(master)
+Eingabe_Amplitude_HM8150_Freq = ttk.Entry(Frame_HM8150_Freq, width=7)
 Eingabe_Amplitude_HM8150_Freq.insert(0,"0.5")
-Eingabe_Frequenz_HM8150_Freq = tk.Entry(master)
+Eingabe_Frequenz_HM8150_Freq = ttk.Entry(Frame_HM8150_Freq, width=7)
 Eingabe_Frequenz_HM8150_Freq.insert(0,"1000")
-Eingabe_Offset_HM8150_Freq = tk.Entry(master)
+Eingabe_Offset_HM8150_Freq = ttk.Entry(Frame_HM8150_Freq, width=7)
 Eingabe_Offset_HM8150_Freq.insert(0,"0")
 
-Label_Output_Button_HM8150_Freq = tk.Label(master, text="Output")
-Label_Offset_Button_HM8150_Freq = tk.Label(master, text="Offset")
+Label_Output_Button_HM8150_Freq = ttk.Label(Frame_HM8150_Freq, text="Output")
+Label_Offset_Button_HM8150_Freq = ttk.Label(Frame_HM8150_Freq, text="Offset")
+Button_Output_on_off_HM8150_Freq = ttk.Button(Frame_HM8150_Freq, text="Off")
+Button_Offset_on_off_HM8150_Freq = ttk.Button(Frame_HM8150_Freq, text="Off")
 
-Button_Output_on_off_HM8150_Freq = tk.Button(master, text="Off")
-Button_Offset_on_off_HM8150_Freq = tk.Button(master, text="Off")
+# HM8150 Design
+Frame_HM8150_Freq.pack()
+Label_Wellenform_HM8150_Freq.grid(column=0, row=0, sticky="W", padx=5, pady=1)
+Label_Amplitude_HM8150_Freq.grid(column=1, row=0, padx=5, pady=1)
+Label_Frequenz_HM8150_Freq.grid(column=2, row=0, padx=5, pady=1)
+Label_Offset_HM8150_Freq.grid(column=3, row=0, padx=5, pady=1)
+Combo_Wellenform_HM8150_Freq.grid(column=0, row=1, sticky="W", padx=5, pady=1)
+Eingabe_Amplitude_HM8150_Freq.grid(column=1, row=1, padx=5, pady=1)
+Eingabe_Frequenz_HM8150_Freq.grid(column=2, row=1, padx=5, pady=1)
+Eingabe_Offset_HM8150_Freq.grid(column=3, row=1, padx=5, pady=1)
+Label_Output_Button_HM8150_Freq.grid(column=1, row=2, padx=5, pady=1)
+Label_Offset_Button_HM8150_Freq.grid(column=2, row=2, padx=5, pady=1)
+Button_Output_on_off_HM8150_Freq.grid(column=1, row=3, padx=5, pady=1)
+Button_Offset_on_off_HM8150_Freq.grid(column=2, row=3, padx=5, pady=1)
 
 
 # Fluke
-Label_Ueberschrift_Fluke = tk.Label(master, text="Multimeter Fluke 8846")
+Frame_Fluke = ttk.LabelFrame(Frame_Steuerung, text="Multimeter Fluke 8846")
 
-Label_Messgroesse_Fluke = tk.Label(master, text="Messgröße")
-Label_Messbereich_Fluke = tk.Label(master, text="Messbereich")
+Label_Messgroesse_Fluke = ttk.Label(Frame_Fluke, text="Messgröße")
+Label_Messbereich_Fluke = ttk.Label(Frame_Fluke, text="Messbereich")
 
 Combo_Messgroesse_Fluke = ttk.Combobox(
-    master,
+    Frame_Fluke,
     state="readonly",
-    values=["Gleichspannung", "Wechselspannung", "Gleichstrom", "Wechselstrom", "Widerstand"]
+    values=["Gleichspannung", "Wechselspannung", "Gleichstrom", "Wechselstrom", "Widerstand"],
+    width=18
 )
 Combo_Messgroesse_Fluke.current(0)
 
 Combo_Messbereich_Fluke = ttk.Combobox(
-    master,
+    Frame_Fluke,
     state="readonly",
-    values=["1V", "10V", "100V", "1000Ohm", "10A"]
+    values=["1V", "10V", "100V", "1000Ohm", "10A"],
+    width=10
 )
 Combo_Messbereich_Fluke.current(0)
 
-Label_Variable_Fluke = tk.Label(master, text="Variable")
-Label_Startwert_Fluke = tk.Label(master, text="Startwert")
-Label_Schrittweite_Fluke = tk.Label(master, text="Schrittweite")
-Label_Zielwert_Fluke = tk.Label(master, text="Zielwert")
+# Fluke Design
+Frame_Fluke.pack()
+Label_Messgroesse_Fluke.grid(column=0, row=0, sticky="W", padx=5, pady=1)
+Label_Messbereich_Fluke.grid(column=1, row=0, padx=5, pady=1)
+Combo_Messgroesse_Fluke.grid(column=0, row=1, sticky="W", padx=5, pady=1)
+Combo_Messbereich_Fluke.grid(column=1, row=1, padx=5, pady=1)
 
-# Variable
+
+# Messung
+Frame_Messung = ttk.LabelFrame(Frame_Steuerung, text="Messung")
+Label_Variable = tk.Label(Frame_Messung, text="Variable")
+Label_Startwert = tk.Label(Frame_Messung, text="Startwert")
+Label_Schrittweite = tk.Label(Frame_Messung, text="Schrittweite")
+Label_Zielwert = tk.Label(Frame_Messung, text="Zielwert")
+
 Combo_Variable = ttk.Combobox(
-    master,
+    Frame_Messung,
     state="readonly",
-    values=["Spannung links", "Spannung rechts"]
+    values=["Spannung links", "Spannung rechts"],
+    width=16
 )
 Combo_Variable.current(0)
 
-Eingabe_Startwert_Variable = tk.Entry(master)
+Eingabe_Startwert_Variable = ttk.Entry(Frame_Messung, width=7)
 Eingabe_Startwert_Variable.insert(0,"0")
-Eingabe_Schrittweite_Variable = tk.Entry(master)
+Eingabe_Schrittweite_Variable = ttk.Entry(Frame_Messung, width=7)
 Eingabe_Schrittweite_Variable.insert(0,"0.03")
-Eingabe_Zielwert_Variable = tk.Entry(master)
+Eingabe_Zielwert_Variable = ttk.Entry(Frame_Messung, width=7)
 Eingabe_Zielwert_Variable.insert(0,"1")
 
-# Messung
-Button_Start_Messung = tk.Button(master, text="Start")
-Button_Stop_Messung = tk.Button(master, text="Stop")
+Label_Auswahl_Parameter = tk.Label(Frame_Messung, text="Parameter Auswahl")
+Label_Eingabe_Parameter = tk.Label(Frame_Messung, text="Eingabe")
 
-graph = tk.PhotoImage(file=r"C:\Users\anton\OneDrive\TU-Berlin\Abschlussarbeit\HLT\graph.png")
+Combo_Parameter = ttk.Combobox(
+    Frame_Messung,
+    state="readonly",
+    values=["Spannung links", "Spannung rechts", "Strom links", "Strom rechts"],
+    width=16
+)
+Combo_Parameter.current(1)
 
-# GRID
-Button_Geraete_lokal.grid(row=0, column=1, columnspan=3, pady=2)
+Eingabe_Parameter = ttk.Entry(Frame_Messung, width=20)
 
+Button_Start_Messung = ttk.Button(Frame_Messung, text="Start")
+Button_Stop_Messung = ttk.Button(Frame_Messung, text="Stop")
 
-Label_Ueberschrift_HM8143_Quelle.grid(row=1, column=1, columnspan=3, pady=2)
+# Messung Design
+Frame_Messung.pack()
+Label_Variable.grid(column=0, row=0, sticky="W", padx=5, pady=1)
+Label_Startwert.grid(column=1, row=0, padx=5, pady=1)
+Label_Schrittweite.grid(column=2, row=0, padx=5, pady=1)
+Label_Zielwert.grid(column=3, row=0, padx=5, pady=1)
+Combo_Variable.grid(column=0, row=1, sticky="W", padx=5, pady=1)
+Eingabe_Startwert_Variable.grid(column=1, row=1, padx=5, pady=1)
+Eingabe_Schrittweite_Variable.grid(column=2, row=1, padx=5, pady=1)
+Eingabe_Zielwert_Variable.grid(column=3, row=1, padx=5, pady=1)
 
-Label_Spannung_links_HM8143_Quelle.grid(row=2, column=0, pady=2)
-Label_Spannung_rechts_HM8143_Quelle.grid(row=2, column=4, pady=2)
+Label_Auswahl_Parameter.grid(column=0, row=2, sticky="W", padx=5, pady=1)
+Label_Eingabe_Parameter.grid(column=1, row=2, padx=5, pady=1)
+Combo_Parameter.grid(column=0, row=3, sticky="W", padx=5, pady=1)
+Eingabe_Parameter.grid(column=1, row=3, sticky="W", columnspan=3, padx=5, pady=1)
 
-Eingabe_Spannung_links_HM8143_Quelle.grid(row=3, column=0, pady=2)
-Eingabe_Spannung_rechts_HM8143_Quelle.grid(row=3, column=4, pady=2)
+Button_Start_Messung.grid(column=0, row=4, columnspan=2, padx=10, pady=10)
+Button_Stop_Messung.grid(column=2, row=4, columnspan=2, padx=10, pady=10)
 
-Label_Strom_links_HM8143_Quelle.grid(row=4, column=0, pady=2)
-Label_Strom_rechts_HM8143_Quelle.grid(row=4, column=4, pady=2)
-
-Eingabe_Strom_links_HM8143_Quelle.grid(row=5, column=0, pady=2)
-Button_on_off_HM8143_Quelle.grid(row=5, column=1, columnspan=3, pady=2)
-Eingabe_Strom_rechts_HM8143_Quelle.grid(row=5, column=4, pady=2)
-
-
-Label_Ueberschrift_HM8150_Freq.grid(row=6, column=1, columnspan=3, pady=2)
-
-Label_Wellenform_HM8150_Freq.grid(row=7, column=0, pady=2)
-Label_Amplitude_HM8150_Freq.grid(row=7, column=2, pady=2)
-Label_Frequenz_HM8150_Freq.grid(row=7, column=3, pady=2)
-Label_Offset_HM8150_Freq.grid(row=7, column=4, pady=2)
-
-Combo_Wellenform_HM8150_Freq.grid(row=8, column=0, pady=2)
-Eingabe_Amplitude_HM8150_Freq.grid(row=8, column=2, pady=2)
-Eingabe_Frequenz_HM8150_Freq.grid(row=8, column=3, pady=2)
-Eingabe_Offset_HM8150_Freq.grid(row=8, column=4, pady=2)
-
-Label_Output_Button_HM8150_Freq.grid(row=9, column=0, pady=2)
-Label_Offset_Button_HM8150_Freq.grid(row=9, column=1, pady=2)
-
-Button_Output_on_off_HM8150_Freq.grid(row=10, column=0, pady=2)
-Button_Offset_on_off_HM8150_Freq.grid(row=10, column=1, pady=2)
-
-
-Label_Ueberschrift_Fluke.grid(row=11, column=1, columnspan=3, pady=2)
-
-Label_Messgroesse_Fluke.grid(row=12, column=0, pady=2)
-Label_Messbereich_Fluke.grid(row=12, column=3, pady=2)
-
-Combo_Messgroesse_Fluke.grid(row=13, column=0, pady=2)
-Combo_Messbereich_Fluke.grid(row=13, column=3, pady=2)
-
-Label_Variable_Fluke.grid(row=14, column=0, pady=2)
-Label_Startwert_Fluke.grid(row=14, column=2, pady=2)
-Label_Schrittweite_Fluke.grid(row=14, column=3, pady=2)
-Label_Zielwert_Fluke.grid(row=14, column=4, pady=2)
-
-Combo_Variable.grid(row=15, column=0, pady=2)
-Eingabe_Startwert_Variable.grid(row=15, column=2, pady=2)
-Eingabe_Schrittweite_Variable.grid(row=15, column=3, pady=2)
-Eingabe_Zielwert_Variable.grid(row=15, column=4, pady=2)
-
-Button_Start_Messung.grid(row=16, column=0, columnspan=2, pady=2)
-Button_Stop_Messung.grid(row=16, column=4, columnspan=2, pady=2)
-
-plot(master)
+plot(Frame_Plot)
 
 master.mainloop()
+
 
